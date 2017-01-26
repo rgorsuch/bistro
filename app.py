801c5c827dfce29d204818c76cf31080cf156cf2
Abhijit Sharma <abhijit.sharma@echostar.com>
2017-01-25 17:54:46 -0700

    made changes to app.py, so as to send JSON instead of text/html to the browser. Also corrected it to be rightly validated by JSLint

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 
 from flask import Flask
+from flask import jsonify
 
 
 app=Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "{ \"project\":\"bistro\" \"version\":\"1.0.0\"}"
+	return jsonify({'project':'bistro', 'version':'1.0.0'})
+    #return "{ \"project\":\"bistro\", \"version\":\"1.0.0\"}"
 
 if __name__=="__main__":
     app.run()
