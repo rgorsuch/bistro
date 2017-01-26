9ebead2a517c66d65e82e1f6c255cbace5e5cc03
Abhijit Sharma <abhijit.sharma@echostar.com>
2017-01-25 18:46:27 -0700

    created a test_app.py file to add unit tests, to be tested by node

---
+++ tests/test_app.py
@@ -0,0 +1,22 @@
+import unittest
+from app import app
+from pprint import pprint
+from flask import jsonify
+import json
+
+class FlaskTest(unittest.TestCase):
+
+    def setUp(self):
+        self.app=app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_root(self):
+        rv=self.app.get('/')
+        pprint(rv)
+        pprint(rv.data)
+        expected_value = {"project": "bistro","version": "1.0.0"}
+        actual_value = json.loads(rv.data)
+        self.assertDictEqual(expected_value, actual_value, "Check that the response is structurally equal")
+
