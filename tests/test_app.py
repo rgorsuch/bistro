278b0dfcbf203af0d6c2aec69992b8bdc62cd8ee
Abhijit Sharma <abhijit.sharma@echostar.com>
2017-01-29 18:27:41 -0700

    Added to app.py and test_app.py cases to include PUT, POST and DELETE

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import unittest
 from app import app
 from pprint import pprint
-from flask import jsonify
+from flask import jsonify, request
 import json
 
 class FlaskTest(unittest.TestCase):
@@ -17,4 +17,17 @@ class FlaskTest(unittest.TestCase):
         expected_value = {"project": "bistro","version": "1.0.0"}
         actual_value = json.loads(rv.data)
         self.assertDictEqual(expected_value, actual_value, "Check that the response is structurally equal")
+
+    def test_post(self):
+        response=self.app.post('/')
+        assert response.status=="200 OK"
+
+    def test_put(self):
+        response=self.app.put('/')
+        assert response.status=="200 OK"
+
+    def test_del(self):
+        response=self.app.delete('/')
+        assert response.status=="200 OK"
+
 
